9b1ae01eaae71e5e84e8770fbbf97b4ac27a5a4e
Your Name <you@example.com>
2023-01-30 17:30:06 -0300

    top and progress bar

--- Projetos/MBike/script/script.js
+++ Projetos/MBike/script/script.js
@@ -73,3 +73,25 @@ function Sair(){
     document.querySelector('html').style.overflow = 'auto'; 
 
 }
+
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#FF6601 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
--- js/script.js
+++ js/script.js
@@ -14,3 +14,11 @@ function reveal(){
     });
 }
 window.addEventListener("scroll",reveal);
+
+let elem = document.getElementById('perc')
+let doc = document.documentElement
+
+window.addEventListener('scroll', function() {
+    let value = parseInt(100 * doc.scrollTop / (doc.scrollHeight - doc.clientHeight));
+    elem.style.width = value + '%';
+})
