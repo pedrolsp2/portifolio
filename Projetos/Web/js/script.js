8f0f3a8cd173b14a6b5ff6255523e46b5ba753aa
Your Name <you@example.com>
2023-02-10 17:41:51 -0300

    web

--- Projetos/Web/js/script.js
+++ Projetos/Web/js/script.js
@@ -4,4 +4,21 @@ let doc = document.documentElement
 window.addEventListener('scroll', function() {
     let value = parseInt(100 * doc.scrollTop / (doc.scrollHeight - doc.clientHeight));
     elem.style.width = value + '%';
-})
+});
+
+function reveal(){
+    var reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach((reveal)=>{
+        var windowHeight = window.innerHeight;
+        var elementTop = reveal.getBoundingClientRect().top;
+        var elementVisible = 100;
+
+        if(elementTop <windowHeight - elementVisible){
+            reveal.classList.add("rolar");
+        } else {
+            reveal.classList.remove("rolar");
+        }
+    });
+}
+window.addEventListener("scroll",reveal);
