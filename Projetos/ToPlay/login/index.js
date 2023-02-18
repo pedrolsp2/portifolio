70be5ceb5c9e261e78d60daba3ad521fab466716
pedrolsp2 <54607320+pedrolsp2@users.noreply.github.com>
2023-02-18 14:57:55 -0300

    relatorios

--- Projetos/ToPlay/login/index.js
+++ Projetos/ToPlay/login/index.js
@@ -42,6 +42,29 @@ function validaCampos() {
         
     }, 1)
     }
+    else if(user !== "Pedro" || psw !== "pedro"){
+        setTimeout(() => {
+            btn.setAttribute("disabled", true);
+            alert.classList.toggle('alert-fim');
+            porcent.style.animation = 'movie';
+            porcent.style.animationDuration = '4s';
+            porcent.style.animationDelay = '400ms';
+            porcent.style.backgroundColor = "#c50d00";
+            h3.style.color = "#d70000";
+            h3.innerHTML = "Ops, correu um erro";
+            txt.innerHTML = "E-mail ou senha incorretos!"
+
+            setTimeout(() => {
+                btn.removeAttribute("disabled");
+                alert.classList.toggle('alert-fim');
+                porcent.classList.toggle('movie');
+                porcent.style.animation = '';
+                porcent.style.animationDuration = '';
+                porcent.style.animationDelay = '';
+        }, 4000);
+        
+    }, 1)
+    }
     else{
         setTimeout(() => {
             alert.classList.toggle('alert-fim');
@@ -62,6 +85,9 @@ function validaCampos() {
         }, 4000);
         
     }, 1)
+    setTimeout(()=>{
+    window.location.href = '../maneger-page/index.html';
+    },4000)
     }
  }
 
--- Projetos/ToPlay/maneger-page/script/index.js
+++ Projetos/ToPlay/maneger-page/script/index.js
@@ -9,10 +9,12 @@ function gerarRelatorio(){
     botao.innerHTML = "Carregando"
     botao.classList.add('back')
         setTimeout(()=>{
+            lod.classList.add('Nvisivel')
+            botao.innerHTML = "Gerar relatório"
+            botao.classList.remove('back')
             btn.classList.toggle('visivel') 
             grafico.classList.toggle('visivel') 
             botao.classList.toggle('Nvisivel')
-            lod.classList.toggle('Nvisivel')
         },2000)
     },1) 
 }
@@ -29,8 +31,35 @@ setTimeout(()=>{
     botao2.innerHTML = "Carregando"
     botao2.classList.add('back')
     setTimeout(()=>{
+        lod2.classList.add('Nvisivel')
+        botao2.innerHTML = "Gerar relatório"
+        botao2.classList.remove('back')
         btn2.classList.toggle('visivel') 
         grafico2.classList.toggle('visivel')
         botao2.classList.toggle('Nvisivel')},2000)
 },1) 
+}
+
+function closeRel(){
+    const botao = document.querySelector('#b1');
+    const lod =  document.querySelector('.loader');
+    const btn = document.querySelector('.btn');
+    const grafico = document.querySelector('.img-grafico');  
+
+    btn.classList.toggle('visivel');
+    grafico.classList.toggle('visivel');
+    botao.classList.replace("Nvisivel",'visivel')
+
+}
+
+function closeRel2(){
+    const botao2 = document.querySelector('#b2');
+    const lod2 =  document.querySelector('.loader2');
+    const btn2 = document.querySelector('.btn2');
+    const grafico2 = document.querySelector('.img-grafico2');  
+
+    btn2.classList.toggle('visivel');
+    grafico2.classList.toggle('visivel');
+    botao2.classList.replace("Nvisivel",'visivel')
+
 }
