5460b03c2894ad0ca55b25cb65a9f6199f2b2a23
pedrolsp2 <54607320+pedrolsp2@users.noreply.github.com>
2023-03-04 09:43:52 -0300

    calourada

--- Projetos/Calourada/js/index.js
+++ Projetos/Calourada/js/index.js
@@ -11,3 +11,17 @@ function menu() {
     var curso = document.getElementById(id);
     curso.classList.toggle('rotate')
  }
+
+ const backToTopButton = document.querySelector("#back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
