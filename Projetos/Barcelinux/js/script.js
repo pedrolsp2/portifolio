faf93ee0bb6bef5d936db71761d509e059c7b484
Your Name <you@example.com>
2023-03-09 17:12:39 -0300

    barcelinux

---
+++ Projetos/Barcelinux/js/script.js
@@ -0,0 +1,8 @@
+function menu() {
+           
+    const navMenu = document.getElementById('nav');
+    navMenu.classList.toggle('toggle')
+    
+     const btnMenu = document.getElementById('botao-menu');
+     btnMenu.classList.toggle('active')
+ }
